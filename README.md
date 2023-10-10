203f00b300e55761740b1b753fcdc304f277f251
Shaler <jake.wm12@gmail.com>
2023-10-10 14:09:01 -0400

    ArrayStack
    i will do a branch for each type of stack i want to implement to practice git and basic data structures

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# OS-learning
- building an OS for learning purposes
+# OSrelatedLearning-and-dataStructures
+ building a base interacting with the client, and data structures that the client can use
+ I want to implement
+ a array stack.
+ and a linked list stack.
+ then an ADTstack.
+
+ ..
+
+ also try an making an arena ive heard its better to malloc and free in blocks rather than keep track of
+ each indiviual piece of memory. (freeing all with one free call);
+
+
+
+ naming conventions im going to use :
+
+ MY_DEFINE
+
+ MyType
+
+ my_function()
+
+ my_variable
+
+
--- TestClient.c
+++ TestClient.c
@@ -1,9 +1,43 @@
 #include "Base.h"
-#include "type_changes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "type_changes.h" //includes static defined as PRIVATE
+#include "arraystack.c" //includes <stdbool.h> and stack.h
 
 
 int main(int argc, char *argv[]) {
 
+    /*basic arraystack client interaction*/
+    int x = 0;
+    printf("hello, client\n");
+    stack_array_push(20);
+
+    x = stack_array_pop();
+    printf("%d \n", x);
+
+    bool check;
+    check = stack_array_is_full();
+    if(check == true)
+        printf("stack is full\n");
+    else {
+        printf("stack is not full\n");
+        stack_array_push(20);
+        stack_array_push(12);
+        stack_array_push(14);
+        stack_array_push(13);
+        stack_array_push(11);
+    }
+
+    while(stack_array_is_empty() == false) {
+        int y = 0;
+        y = stack_array_pop();
+        printf("y = %d\n", y);
+    }
+    /*end of arraystack tests*/
+
+
+
+    /*linked list version*/
 
 
 }
